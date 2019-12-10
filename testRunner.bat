8080055c685536cbd67555b48e979fb0f2535cca
Martinsh Gailums <Martinsh_Gailums@rcomext.com>
2019-12-10 15:54:51 +0200

    allure reporting added + test execution .bat file

---
+++ testRunner.bat
@@ -0,0 +1,11 @@
+@echo off
+echo 1) chrome = 1
+echo 2) firefox = 2
+set /p number=Choose Browser: 
+if %number% == "1" (
+   set browser="chrome"
+)
+if  %number% == 2 (
+   set browser="firefox"
+)
+mvn test -Dbrowser=%browser% & allure generate --clean & xcopy /e/y allure-report\history allure-results\history && allure serve --host localhost
